f77019142404752af053aff048d378d70035d593
Samir Makhani <makhani@berkeley.edu>
2014-02-08 13:54:06 -0800

    added home page

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return "No illegal activity will occur on this site."
+
 @app.route('/downloads/', methods=('GET', 'POST'))
 def get_link():
     results = {}
-    if request.method == "POST":
+    if request.method == "GET":
         client_json = request.json
-        results['url'] = get_download_url
+        results['url'] = get_download_url(client_json)
     return jsonify(dict=results)
 
 def get_download_url(song_info_dict):
     return ""
+
+if __name__ == '__main__':
+    app.run()
