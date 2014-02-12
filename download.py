ce123b886dc97b192b856f4328afec992a0822a4
Song Sok <song.sok@berkeley.edu>
2014-02-11 22:28:36 -0800

    updated download.py

--- download.py
+++ download.py
@@ -11,15 +11,21 @@ def scrap(dict):
 	temp_url = mp3_url + url + ".html"
 	soup = BeautifulSoup(urllib2.urlopen(temp_url).read())
 	songs = soup.findAll('div', {'id':'song_html'})
-	download_link = songs.findAll('a', {'style' : 'color:green;'})
-	final_link = download_link[0].get('href')
-	meta_data.append(final_link)
-	return final_link
+	counter = 1
+	for song in songs:
+		temp_lst = extract_meta(song)
+		link = song.findAll('a', {'style' : 'color:green;'})
+		download_link = link[0].get('href')
+		download_link.replace("u'^", '')
+		temp_lst.append(download_link)
+		final_meta_data[str(counter)] = temp_lst
+		counter += 1
+	return final_meta_data
 
 ## Returns a list of meta data from mp3skull
 def extract_meta(tag):
 	final_meta_list = []
-	meta = str(tag[0].findAll('div', {'class':'left'})).split('<br/>')
+	meta = str(tag.findAll('div', {'class':'left'})).split('<br/>')
 	bitrate = meta[0].split('\t')
 	time = meta[1]
 	filesize = meta[2].split('\t')[0]
